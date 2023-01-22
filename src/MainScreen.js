f495bed1189b54c19bf062e63b39ce5923c41213
김주연 <1502home@naver.com>
2023-01-22 21:05:46 +0900

    [IOS] statusBar 정보 표기 색상 옵션 추가

--- src/MainScreen.js
+++ src/MainScreen.js
@@ -183,11 +183,18 @@ export const MainScreen = (props) => {
     setDate(day);
   }
 
-  
+  const barColor = () => {
+    if (Platform.OS === 'ios'){
+    return(
+      <StatusBar barStyle="dark-content" />
+    )
+    }
+  }
+
   return(
     /** 전체 화면 표기 부분 */
     <SafeAreaView>
-      <StatusBar barStyle="default" />
+      {barColor()}
       <View style = {styles.maincontainer}>
         <View style = {styles.container_topbar}>
           <Text style={{color: 'white', fontSize: 16, marginLeft:30}}>
