45b7e7a7d25d7dcce6b14688029388720ad4b497
김주연 <1502home@naver.com>
2023-01-25 12:28:53 +0900

    [IOS] 일부 앱이 Apple의 SKAdNetwork를 사용하도록 구성되지 않았습니다. 앱 설치와 같은 모든 광고 활동을 인정받으려면 Google의 추천 네트워크 ID로 SKAdNetwork를 구성해야 합니다. <- 해결

--- App.js
+++ App.js
@@ -1,15 +1,36 @@
 import React, { useEffect, useState } from 'react';
 import { MainScreen } from './src/MainScreen';
-import {StyleSheet, View, Text, Image, TouchableOpacity, ScrollView, SafeAreaView,} from 'react-native';
+import {StyleSheet, View, Text, Image, TouchableOpacity, ScrollView, SafeAreaView, AppState, Platform } from 'react-native';
 
 import axios from 'axios'; //For pbnb API Test
 import mobileAds from 'react-native-google-mobile-ads';
+import {request, PERMISSIONS} from 'react-native-permissions';
+import { requestTrackingPermission } from 'react-native-tracking-transparency';
 
 // Google 광고를 초기화합니다. 반드시 App.js에 정의해야 해요.
 mobileAds().initialize().then(adapterStatuses => {});
 
 export const App = () => {
   const [TrgtDate, setTrgtDate] = useState(new Date());
+
+  const checkPermissionForIOS = async () => {
+    return await requestTrackingPermission();
+  }
+
+  useEffect(() => {
+    checkPermissionForIOS();
+  },[]);
+
+  useEffect(() => {
+    const listener = AppState.addEventListener('change', (status) => {
+      if (Platform.OS === 'ios' && status === 'active') {
+        request(PERMISSIONS.IOS.APP_TRACKING_TRANSPARENCY)
+          .then((result) => console.warn(result))
+          .catch((error) => console.warn(error));
+      }
+    });
+    return () => {listener.remove()}
+  }, []);
 
   /**Screen 화면 Manage 해주는 함수에요 */
   const getScreen = () =>{
